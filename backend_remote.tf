b633e094046ec0930098bf660c3c1d767608ba8e
Your Name <you@example.com>
2022-03-27 12:18:01 +0530

    chnged

---
+++ backend_remote.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "myfirstbucketforcheck"
+    key    = "devops.statefile"
+    region = "us-west-1"
+  }
+}
--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = "us-east-1"
+  region     = "us-west-1"
   access_key = "AKIAXXFEOCX4P73ZBKI3"
   secret_key = "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"
 }
