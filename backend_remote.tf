9ec707f7b5f049f9145880a2b0ab0b6d96879404
Your Name <you@example.com>
2022-04-05 09:55:19 +0530

    updated files

--- backend_remote.tf
+++ backend_remote.tf
@@ -4,4 +4,7 @@ terraform {
     key    = "devops.statefile"
     region = "us-west-1"
   }
+  depends_on = [
+    aws_s3_bucket.my_s3_bucket,
+  ]
 }
--- ec2.tf
+++ ec2.tf
@@ -7,4 +7,3 @@ resource "aws_instance" "AWSServer" {
     Name = "terraform ${( count.index+1 )}"
   }
 }
-
