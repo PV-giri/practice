013e59db2a7f4df7c9fcc8c0a0a39b915c3c5f87
Your Name <you@example.com>
2022-04-08 10:47:31 +0530

    updated file

--- ec2.tf
+++ ec2.tf
@@ -3,17 +3,15 @@ resource "aws_instance" "AWSServer" {
   ami           = "${var.ami}"
   instance_type = "t2.micro"
   key_name      = "jenkins"
-  
   user_data = << EOF
 		#! /bin/bash
-                sudo apt-get update
+    sudo apt-get update
 		sudo apt-get install -y apache2
 		sudo systemctl start apache2
 		sudo systemctl enable apache2
 		echo "<h1>Deployed via Terraform-${( count.index+1 )}</h1>" | sudo tee /var/www/html/index.html
 	EOF
-
-  tags = {
+    tags = {
     Name = "terraform ${( count.index+1 )}"
   }
 }
--- s3_backet.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_s3_bucket" "my_s3_bucket" {
-  bucket = "mysecondbucketforcheck"
-
-  tags = {
-    Name        = "My-second-bucket"
-  }
-} 
