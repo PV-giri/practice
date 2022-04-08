c9a5ca212d8e011956d9a2fc52f8bd5462aebdaf
Your Name <you@example.com>
2022-04-08 10:52:29 +0530

    updated file

--- ec2.tf
+++ ec2.tf
@@ -3,13 +3,13 @@ resource "aws_instance" "AWSServer" {
   ami           = "${var.ami}"
   instance_type = "t2.micro"
   key_name      = "jenkins"
-  user_data = << EOF
+  /* user_data = << EOF
 		#! /bin/bash
     sudo apt-get update
 		sudo apt-get install -y apache2
 		sudo systemctl start apache2
 		sudo systemctl enable apache2
-	EOF
+	EOF */
     tags = {
     Name = "terraform ${( count.index+1 )}"
   }
