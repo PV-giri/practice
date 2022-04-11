e52c1d9921f80eecab1f3503704fe91aecbb62ca
Your Name <you@example.com>
2022-04-11 09:29:10 +0530

    updated files

--- ec2.tf
+++ ec2.tf
@@ -1,25 +1,25 @@
-resource "aws_instance" "AWSServer" {
-  /* count = "${var.Env == "prod" ?  3:1}" */
-  count = 2
-  ami           = "${var.ami}"
-  instance_type = "t2.micro"
-  key_name      = "jenkins"
-   /* user_data = << EOF
-  #! /bin/bash
-  sudo apt-get update
-  sudo apt-get install -y apache2
-  sudo systemctl start apache2
-  sudo systemctl enable apache2
-  EOF */
-   lifecycle {
-    create_before_destroy = true
-    prevent_destroy = false
-    /* ignore_changes = [
-      tags,
-    ] */
+  resource "aws_instance" "AWSServer" {
+    /* count = "${var.Env == "prod" ?  3:1}" */
+    count = 2
+    ami           = "${var.ami}"
+    instance_type = "t2.micro"
+    key_name      = "jenkins"
+    /* user_data = << EOF
+    #! /bin/bash
+    sudo apt-get update
+    sudo apt-get install -y apache2
+    sudo systemctl start apache2
+    sudo systemctl enable apache2
+    EOF */
+    lifecycle {
+      create_before_destroy = true
+      prevent_destroy = false
+      /* ignore_changes = [
+        tags,
+      ] */
+    }
+      tags = {
+      Name = "terraform ${( count.index+1 )}"
+      /* Env = "${var.Env}" */
+    }
   }
-    tags = {
-    Name = "terraform ${( count.index+1 )}"
-    /* Env = "${var.Env}" */
-  }
-}
